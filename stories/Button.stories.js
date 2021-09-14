8880bb94eb5a988b8fd60542209fe9b2f4abb149
James Champ <jameschamp@acm.org>
2021-09-14 11:49:24 -0400

    Update storybook

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -16,19 +16,16 @@ const ButtonTypes = {
     preview: ' cta-btn--shell cta-btn--preview'
 }
 
-export const CtaBtn = () => ButtonTemplate('cta-btn','Leave waitlist');
+export const CtaBtn = () => ButtonTemplate('default','Leave waitlist');
 
-export const CtaBtnUnavailable = () => ButtonTemplate('cta-btn cta-btn--unavailable','Join waitlist');
+export const CtaBtnUnavailable = () => ButtonTemplate('unavailable','Join waitlist');
 
-export const CtaBtnAvailable = () => ButtonTemplate('cta-btn cta-btn--available','Borrow');
+export const CtaBtnAvailable = () => ButtonTemplate('available','Borrow');
 
-export const CtaBtnPreview = () => ButtonTemplate('cta-btn cta-btn--preview','Preview');
+export const CtaBtnPreview = () => ButtonTemplate('preview','Preview');
 
 export const CtaBtnWithBadge = () =>
-    ButtonTemplate('cta-btn cta-btn--unavailable',
-        `Join waiting list
-    <span class="cta-btn__badge">4</span>`)
-;
+    ButtonTemplate('unavailable', 'Join waiting list', 4);
 
 export const CtaBtnGroup = () => `<div class="cta-button-group">
 <a href="/borrow/ia/sevenhabitsofhi00cove?ref=ol" title="Borrow ebook from Internet Archive" id="borrow_ebook" data-ol-link-track="CTAClick|Borrow" class="cta-btn cta-btn--available">Borrow</a>
