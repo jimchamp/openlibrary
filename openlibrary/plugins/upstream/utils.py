59259857fb766121ea11647d3998aeb656472113
acaligac <caligacal.ayan@gmail.com>
2026-05-01 05:13:43 -0400

    web.safeunicode (#12520)

--- openlibrary/plugins/upstream/utils.py
+++ openlibrary/plugins/upstream/utils.py
@@ -1270,7 +1270,7 @@ class HTML(str):
     __slots__ = ()
 
     def __init__(self, html):
-        str.__init__(self, web.safeunicode(html))
+        str.__init__(self, str(html))
 
     def __repr__(self):
         return "<html: %s>" % str.__repr__(self)
@@ -1328,10 +1328,7 @@ class UpstreamMemcacheClient:
         keys = [web.safestr(k) for k in keys]
 
         d = self._client.get_multi(keys)
-        return {
-            web.safeunicode(adapter.unconvert_key(k)): self.decompress(v)
-            for k, v in d.items()
-        }
+        return {str(adapter.unconvert_key(k)): self.decompress(v) for k, v in d.items()}
 
 
 if config.get('upstream_memcache_servers'):
